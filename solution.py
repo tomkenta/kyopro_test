bb83f89d2d32dde59433a34f4c24215eddd0494a
tomkenta <tomkenta2014@gmail.com>
2018-03-09 19:33:28 +0900

    [FIX] 期待値と実測値の順番変更

--- solution.py
+++ solution.py
@@ -2,17 +2,26 @@
 
 ##### solveの中に記述，提出時はsolve()のコメントアウトを外して
 ##### ここから
+### 1000 = 1km
 def solve():
-    a, b, c = list(map(int, input().split()))
-    if a <= c and c <= b:
-        print("YES")
-    else:
-        print("NO")
+    m = int(input())
+    vv = 0
+    if m < 100:
+        vv = 0
+    elif 100 <= m and m <= 5000:
+        vv = m * 10
+    elif 6000 <= m and m <= 30000:
+        vv = m + 50000
+    elif 35000 <= m and m <= 70000:
+        vv = ((m - 30000) / 5) + 80000
+    elif 70000 < m:
+        vv = 89000
+
+    output = int(vv / 1000)
+    print("{0:02d}".format(output))
+
 
 # solve()
 ##### ここまで
-
-
-
-
-
+if __name__ == '__main__':
+    solve()
--- testcode.py
+++ testcode.py
@@ -21,17 +21,18 @@ class MyTestCase(unittest.TestCase):
     files = sorted(os.listdir(test_path))
 
     """
-    input: 3 2 1
-    expect output: NO
+    input: 15000
+    expect output: 65
     """
     def test_case0(self):
         path = self.test_path + '/' + self.files[0]
-        expected_output = "NO"
+        expected_output = "65"
         with open(path) as f:
             input = f.readlines()
 
         print("testing: {}".format(path))
         print("input:\n{}".format(''.join(input)))
+        print("expected output:\n{}".format(expected_output))
 
         fd = os.open(path, os.O_RDONLY)
         os.dup2(fd,sys.stdin.fileno())
@@ -40,22 +41,22 @@ class MyTestCase(unittest.TestCase):
             solve()
             lines = stdout.getvalue().splitlines()
             # 期待するstdout 複数行あるときは注意
-            self.assertEqual(lines[0], expected_output)
-
+            self.assertEqual(expected_output,lines[0])
         os.close(fd)
 
     """
-    input: 1 3 2
-    expect output: YES
+    input: 75000
+    expect output: 89
     """
     def test_case1(self):
         path = self.test_path + '/' + self.files[1]
-        expected_output = "YES"
+        expected_output = "89"
         with open(path) as f:
             input = f.readlines()
 
         print("testing: {}".format(path))
         print("input:\n{}".format(''.join(input)))
+        print("expected output:\n{}".format(expected_output))
 
         fd = os.open(path, os.O_RDONLY)
         os.dup2(fd,sys.stdin.fileno())
@@ -64,8 +65,53 @@ class MyTestCase(unittest.TestCase):
             solve()
             lines = stdout.getvalue().splitlines()
             # 期待するstdout 複数行あるときは注意
-            self.assertEqual(lines[0], expected_output)
+            self.assertEqual(expected_output,lines[0])
+        os.close(fd)
 
+    """
+    input: 200
+    expect output: 02
+    """
+    def test_case2(self):
+        path = self.test_path + '/' + self.files[2]
+        expected_output = "02"
+        with open(path) as f:
+            input = f.readlines()
+
+        print("testing: {}".format(path))
+        print("input:\n{}".format(''.join(input)))
+        print("expected output:\n{}".format(expected_output))
+
+        fd = os.open(path, os.O_RDONLY)
+        os.dup2(fd,sys.stdin.fileno())
+
+        with captured_stdout() as stdout:
+            solve()
+            lines = stdout.getvalue().splitlines()
+            self.assertEqual(expected_output,lines[0])
+        os.close(fd)
+
+    """
+    input: 40000
+    expect output: 82
+    """
+    def test_case3(self):
+        path = self.test_path + '/' + self.files[3]
+        expected_output = "82"
+        with open(path) as f:
+            input = f.readlines()
+
+        print("testing: {}".format(path))
+        print("input:\n{}".format(''.join(input)))
+        print("expected output:\n{}".format(expected_output))
+
+        fd = os.open(path, os.O_RDONLY)
+        os.dup2(fd,sys.stdin.fileno())
+
+        with captured_stdout() as stdout:
+            solve()
+            lines = stdout.getvalue().splitlines()
+            self.assertEqual(expected_output,lines[0])
         os.close(fd)
 
 if __name__ == '__main__':
